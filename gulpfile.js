9077154ad9f5cab2e842d6c410eb2e00267c3094
Ilya Kirillov <ilyakirillov97@yandex.ru>
2020-08-24 23:40:13 +0300

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@ const gulp = require('gulp')
 const plumber = require('gulp-plumber')
 const sass = require('gulp-sass')
 const sassGlob = require('gulp-sass-glob')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const inlineSvg = require('postcss-inline-svg')
+
+const postcssPlugins = [
+  autoprefixer(),
+  inlineSvg()
+]
 
 function compileSass() {
   return gulp.src('src/morkovka.scss')
@@ -13,6 +21,7 @@ function compileSass() {
     }))
     .pipe(sassGlob())
     .pipe(sass())
+    .pipe(postcss(postcssPlugins))
     .pipe(gulp.dest('dist'))
 }
 exports.compileSass = compileSass
